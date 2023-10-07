8473e5182f69e58b3931b42d83282c34978d3a71
paleto30 <paleto0817@gmail.com>
2023-10-07 15:35:02 -0500

    feat (model) : :rocket: modelo de estudiantes , se agrego el rol

--- src/models/estudiante.model.js
+++ src/models/estudiante.model.js
@@ -4,39 +4,39 @@ import db from "../db/conexion.js";
 
 
 // modelo de estudiante 
-const EstudianteModel = db.define('estudiante',{
-    id:{
+const EstudianteModel = db.define('estudiante', {
+    id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
     },
-    doc_id:{
+    doc_id: {
         type: DataTypes.STRING(20),
         allowNull: false,
         unique: true
     },
-    nombre:{
+    nombre: {
         type: DataTypes.STRING(255),
         allowNull: false,
-        validate:{
+        validate: {
             len: [3, 255]
         }
     },
-    apellido:{
+    apellido: {
         type: DataTypes.STRING(255),
         allowNull: false,
-        validate:{
+        validate: {
             len: [3, 255]
         }
     },
-    telefono:{
+    telefono: {
         type: DataTypes.STRING(20),
         allowNull: false,
-        validate:{
+        validate: {
             len: [6, 20]
         }
     },
-    correo:{
+    correo: {
         type: DataTypes.STRING(100),
         allowNull: false,
         unique: true,
@@ -45,22 +45,31 @@ const EstudianteModel = db.define('estudiante',{
             len: [10, 100]
         }
     },
-    clave:{
+    clave: {
         type: DataTypes.STRING(300),
         allowNull: false,
         validate: {
             len: [8, 255]
         }
     },
-    id_carrera:{
+    id_carrera: {
         type: DataTypes.INTEGER,
         allowNull: false,
         references: {
             model: 'tg_carrera',
             key: 'id'
         },
-    },   
-},{
+    },
+    rol: {
+        type: DataTypes.SMALLINT,
+        allowNull: false,
+        defaultValue: 3,
+        validate: {
+            is: [3]
+        }
+
+    }
+}, {
     tableName: 'tg_estudiante',
     timestamps: true,
     underscored: true
