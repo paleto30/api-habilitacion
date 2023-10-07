2d902ea994ab2e5a1630cc92ac75cd17da7a39b6
paleto30 <paleto0817@gmail.com>
2023-10-07 15:38:06 -0500

    feat (routes): :rocket: rutas para le gestion de autenticacion
    llamados middlewares de autenticacion

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
 import authController from "../controllers/auth.controller.js";
+import verifyToken from '../middlewares/verifyAccessToken.js';
+import { checkUserRole } from '../middlewares/checkRole.js';
+
+
+
+
+
 
 
 // creamos un router
@@ -7,24 +14,27 @@ const router = Router();
 
 
 /*  ruta para obtener la lista de sedes disponibles */
-router.get('/sedes',[], authController.getSedes);
+router.get('/sedes', [], authController.getSedes);
 
 /* ruta para obtener la lista de facultades correspondientes a la sede seleccionada */
-router.get('/sede/:id_sede/facultades',[],authController.getFacultadesBySede);
+router.get('/sede/:id_sede/facultades', [], authController.getFacultadesBySede);
 
 /* ruta para obtener la lista de coordinaciones */
-router.get('/facultad/:id_facultad/coordinaciones',[], authController.getCoordinacionByFacultad);
+router.get('/facultad/:id_facultad/coordinaciones', [], authController.getCoordinacionByFacultad);
 
 /* ruta paa obtener la lista de carreras correspondientes a la coordinacion */
-router.get('/coordinacion/:id_coordinacion/carreras',[],authController.getCarrerasByCoordinacion);
-
+router.get('/coordinacion/:id_coordinacion/carreras', [], authController.getCarrerasByCoordinacion);
 
 
 
 /* ruta para hacer el registro de un estudiante */
-router.post('/registrarse',[],authController.studentRegistration);
+router.post('/registrarse', [], authController.studentRegistration);
 
+/* ruta para hacer el Inicio de sesion de un usuario */
+router.post('/iniciar-sesion', [], authController.loginManagement);
 
+/* ruta para hacer el registro de un admin */
+router.post('/register/user-admin', [verifyToken, checkUserRole(2)], authController.adminRegistration);
 
 
 
