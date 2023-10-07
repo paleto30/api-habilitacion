b93badfbab3f5e067f37bd69af4296a90c35392a
paleto30 <paleto0817@gmail.com>
2023-10-07 15:34:18 -0500

    feat (model): :rocket: modelo de administrador

---
+++ src/models/administrador.model.js
@@ -0,0 +1,78 @@
+import { DataTypes } from "sequelize";
+import db from "../db/conexion.js";
+
+
+
+// modelo de estudiante 
+const AdministradorModel = db.define('administrador', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    doc_id: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        unique: true
+    },
+    nombre: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        validate: {
+            len: [3, 255]
+        }
+    },
+    apellido: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        validate: {
+            len: [3, 255]
+        }
+    },
+    telefono: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        validate: {
+            len: [6, 20]
+        }
+    },
+    correo: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+            len: [10, 100]
+        }
+    },
+    clave: {
+        type: DataTypes.STRING(300),
+        allowNull: false,
+        validate: {
+            len: [8, 255]
+        }
+    },
+    id_coordinacion: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'tg_coordinacion',
+            key: 'id'
+        },
+    },
+    rol: {
+        type: DataTypes.SMALLINT,
+        allowNull: false,
+        defaultValue: 2,
+        validate: {
+            isIn: [[2, 3]]
+        }
+    }
+}, {
+    tableName: 'tg_administrador',
+    timestamps: true,
+    underscored: true
+});
+
+
+export default AdministradorModel;
