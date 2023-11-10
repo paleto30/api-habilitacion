f29b0a7afe0d842364d4b8aecbf9d7116bfd92db
paleto30 <paleto0817@gmail.com>
2023-11-10 00:15:34 -0500

    refactor ( clean text) : :art: limpiando la semantica en las rutas

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import authController from "../controllers/auth.controller.js";
 import verifyToken from '../middlewares/verifyAccessToken.js';
-import { checkUserRole } from '../middlewares/checkRole.js';
+import { checkUserRoles } from '../middlewares/checkRole.js';
 
 
 
@@ -14,27 +14,29 @@ const router = Router();
 
 
 /*  ruta para obtener la lista de sedes disponibles */
-router.get('/sedes', [], authController.getSedes);
+router.get('/campus', [], authController.getCampus);
 
 /* ruta para obtener la lista de facultades correspondientes a la sede seleccionada */
-router.get('/sede/:id_sede/facultades', [], authController.getFacultadesBySede);
+router.get('/campus/:id_sede/faculties', [], authController.getFacultiesByCampus);
 
 /* ruta para obtener la lista de coordinaciones */
-router.get('/facultad/:id_facultad/coordinaciones', [], authController.getCoordinacionByFacultad);
+router.get('/faculties/:id_facultad/coordinations', [], authController.getCoordinationByFacultie);
 
 /* ruta paa obtener la lista de carreras correspondientes a la coordinacion */
-router.get('/coordinacion/:id_coordinacion/carreras', [], authController.getCarrerasByCoordinacion);
+router.get('/coordinations/:id_coordinacion/careers', [], authController.getCareersByCoordination);
+
+
 
 
 
 /* ruta para hacer el registro de un estudiante */
-router.post('/registrarse', [], authController.studentRegistration);
+router.post('/register', [], authController.studentRegistration);
 
 /* ruta para hacer el Inicio de sesion de un usuario */
-router.post('/iniciar-sesion', [], authController.loginManagement);
+router.post('/login', [], authController.loginManagement);
 
 /* ruta para hacer el registro de un admin */
-router.post('/register/user-admin', [verifyToken, checkUserRole(2)], authController.adminRegistration);
+router.post('/register/user-admin', [verifyToken, checkUserRoles([1, 2])], authController.adminRegistration);
 
 
 
