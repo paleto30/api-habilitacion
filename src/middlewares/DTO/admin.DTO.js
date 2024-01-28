8029c10b74ab92aead47beb5a554ba9d459fc344
paleto30 <paleto0817@gmail.com>
2024-01-27 23:15:37 -0500

    feat (schemas y DTO): :zap: se agregaron mensajes personalizados a los esquemas ,

--- src/middlewares/DTO/admin.DTO.js
+++ src/middlewares/DTO/admin.DTO.js
@@ -1,5 +1,5 @@
 import { handlerHttpErrors } from '../../helpers/errorhandler.js'
-import { schemaValidateQueryParamsFilters } from '../../schemas/admins.schema.js'
+import { schemaValidateQueryParamsFilters, schemaValidateRecoveryTable } from '../../schemas/admins.schema.js'
 
 
 
@@ -9,7 +9,21 @@ export const DTOvalidateQueryFilters = async (req, res, next) => {
         req.query.id_coo = req.usuario.id_coordinacion
         const validate = await schemaValidateQueryParamsFilters.validateAsync(req.query);
         req.paramsDTO = validate;
-        console.log(validate);
+        next();
+    } catch (error) {
+        handlerHttpErrors(res, error.message);
+    }
+}
+
+
+
+// DTO para ruta de habilitaciones admin (filtros)
+export const DTOvalidateRecoveryTable = async (req, res, next) => {
+    try {
+        req.query.id_coo = req.usuario.id_coordinacion;
+        const validate = await schemaValidateRecoveryTable.validateAsync(req.query);
+        
+        req.DTO = validate;
         next();
     } catch (error) {
         handlerHttpErrors(res, error.message);
