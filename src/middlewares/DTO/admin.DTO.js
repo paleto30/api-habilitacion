74dd3ed6fb6d793df9ca44ba025f3b27a2207145
paleto30 <paleto0817@gmail.com>
2024-01-30 22:13:29 -0500

    feat (new schema & DTO) : :sparkles: nuevo esquema y DTO para la ruta de obtener detalles de habilitacion

--- src/middlewares/DTO/admin.DTO.js
+++ src/middlewares/DTO/admin.DTO.js
@@ -1,5 +1,5 @@
 import { handlerHttpErrors } from '../../helpers/errorhandler.js'
-import { schemaValidateQueryParamsFilters, schemaValidateRecoveryTable } from '../../schemas/admins.schema.js'
+import { schemaValidateQueryParamsFilters, schemaValidateRecoveryDetails, schemaValidateRecoveryTable } from '../../schemas/admins.schema.js'
 
 
 
@@ -22,7 +22,19 @@ export const DTOvalidateRecoveryTable = async (req, res, next) => {
     try {
         req.query.id_coo = req.usuario.id_coordinacion;
         const validate = await schemaValidateRecoveryTable.validateAsync(req.query);
-        
+
+        req.DTO = validate;
+        next();
+    } catch (error) {
+        handlerHttpErrors(res, error.message);
+    }
+}
+
+
+// DTO para ruta de optener los detalles de la habilitacion
+export const DTOvalidateRecoveryDetails = async (req, res, next) => {
+    try {
+        const validate = await schemaValidateRecoveryDetails.validateAsync(req.params);
         req.DTO = validate;
         next();
     } catch (error) {
